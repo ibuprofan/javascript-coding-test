5dd2d04da036f110d1328e7c09ccd2fdf48bad03
Martin Swiderski <martinswiderski@googlemail.com>
2018-09-19 20:38:03 +0100

    tests

--- tests/specs/sign-test-spec.js
+++ tests/specs/sign-test-spec.js
@@ -19,10 +19,11 @@ function MockConsole () {
 
 let mockConsole = new MockConsole();
 
-require('./../../src/sign-test')(__dirname+'/nvl-spec.js', mockConsole);
+// md5sum ../props/test.js = '058eb02dfd08e347c40ae14e9f2e4600'
+require('./../../src/sign-test')(__dirname+'/../props/test.js', mockConsole);
 
 describe('console can be injected', function () {
     it('string contains a valid MD5', function () {
-        expect(mockConsole.captured().contains('e0d52f79a510ff64e465e2a595ba2468')).toBe(true);
+        expect(mockConsole.captured().contains('058eb02dfd08e347c40ae14e9f2e4600')).toBe(true);
     });
 });
